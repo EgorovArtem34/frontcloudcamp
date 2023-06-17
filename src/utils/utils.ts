274a00b507d84d39e93ae5abc7deffb0b947049c
Artem Egorov <artegtor@gmail.com>
2023-06-17 22:49:42 +0500

    update utils.ts : add myData

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
+import { CheckboxStateType } from '../types';
+
 const calculateProgressBarWidth = (finishedStepsCount: number) => {
   switch (finishedStepsCount) {
     case 1:
@@ -10,5 +12,22 @@ const calculateProgressBarWidth = (finishedStepsCount: number) => {
       return `Invalid finishedStepsCount: ${finishedStepsCount}`;
   }
 };
+
+export const myPhone = '+7 999 999-99-99';
+export const myEmail = 'artegtor@gmail.com';
+
+export const myLinks = [
+  { name: 'Telegram', url: 'https://t.me/artegtor' },
+  { name: 'GitHub', url: 'https://github.com/EgorovArtem34' },
+  { name: 'Resume', url: 'https://career.habr.com/artegtor' },
+];
+
+export const getTrueKeysCheckBoxes = (checkBoxes: Record<number, boolean>): number[] => Object.keys(checkBoxes).reduce<number[]>((acc, key) => {
+  const numericKey = parseInt(key);
+  if (checkBoxes[numericKey]) {
+    acc.push(numericKey);
+  }
+  return acc;
+}, []);
 
 export default calculateProgressBarWidth;
